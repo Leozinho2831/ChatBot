14cfb8b201fc268c412039ba4ffedfc87cd7db10
Leozinho2831 <leoferraz1818@gmail.com>
2025-03-11 13:21:12 -0300

    new fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['src/javascript/*.js', '*.html'],
-  theme: {
-    extend: {
-      screens: {
-        'mobile': '480px',
-        'tablet': '768px',
-        'desktop': '1024px'
-      },
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'], 
-        inter: ['Inter', 'Arial' ,'sans-serif']
-      }
+    content: ['src/javascript/*.js', '*.html'],
+    theme: {
+        extend: {
+            screens: {
+            'mobile': '480px',
+            'tablet': '768px',
+            'desktop': '1024px',
+            },
+            fontFamily: {
+                poppins: ['Poppins', 'sans-serif'], 
+                inter: ['Inter', 'Arial' ,'sans-serif'],
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
-
