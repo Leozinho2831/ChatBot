5ea13ff24bc7445f4fa9c3704d97f3b489676f31
Leozinho2831 <leoferraz1818@gmail.com>
2025-03-13 16:43:17 -0300

    new gray colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,22 @@ module.exports = {
             fontFamily: {
                 poppins: ['Poppins', 'sans-serif'], 
                 inter: ['Inter', 'Arial' ,'sans-serif'],
+            },
+            colors: {
+                gray: {
+                    50: '#F5F7FA',
+                    100: '#E6EAF0',
+                    200: '#CED3D9',
+                    300: '#C0C5CB',
+                    400: '#B2B8BF',
+                    500: '#989EA6',
+                    600: '#7E838C',
+                    700: '#626872',
+                    800: '#474D59',
+                    900: '#363B47',
+                    950: '#242833',
+                    'black': '#0B0C12',
+                }
             }
         },
     },
