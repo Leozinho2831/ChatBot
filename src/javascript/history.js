9ea297cb0e75a08b30d3dae5e6a31a216d3f059f
Leozinho2831 <leoferraz1818@gmail.com>
2025-03-20 21:52:16 -0300

    new code to create history

--- src/javascript/history.js
+++ src/javascript/history.js
@@ -54,10 +54,76 @@ function openHistory(element){
     toggleText(element);
 }
 
-buttonPlus.onclick = () => {
-    openHistory(sectionPlus);
+if(sectionPlus){
+    buttonPlus.onclick = () => {
+        openHistory(sectionPlus);
+    }
 }
 
-buttonLoad.onclick = () => {
-    openHistory(sectionLoad);
+if(sectionLoad){
+    buttonLoad.onclick = () => {
+        openHistory(sectionLoad);
+    }
 }
+
+// trocar texto por criação do gimini
+const message = `message`;
+
+const historyItem = 
+`
+    <div class="flex items-center gap-5 cursor-pointer hoverBg">
+        <div class="space-y-[2px] *:w-4 *:h-[2px] *:bg-gray-500">
+            <div></div>
+            <div></div>
+            <div class="!w-[10px]"></div>
+        </div>
+        <p class="flex-1 text-gray-400 text-base">${message}</p>
+        <figure class="inline-flex items-center justify-center w-8 h-8 !p-0 hoverBg js-options">
+            <img src="src/images/options.svg" alt="options">
+        </figure>
+    </div>
+`;
+
+// colocar block por js quando houver mais de 5 histórico
+const historyItemPlus = 
+`
+    <div class="flex items-center gap-5 cursor-pointer hoverBg js-buttonPlus">
+        <img class="w-4 h-4 js-rotate180" src="src/images/arrow-menu.svg" alt="Mais">
+        <p class="text-gray-400 text-base">Mais</p>
+    </div>
+    <section class="hidden space-y-1 js-sectionPlus">
+        <div class="flex items-center gap-5 cursor-pointer hoverBg">
+            <div class="space-y-[2px] *:w-4 *:h-[2px] *:bg-gray-500">
+                <div></div>
+                <div></div>
+                <div class="!w-[10px]"></div>
+            </div>
+            <p class="flex-1 text-gray-400 text-base">${message}</p>
+            <figure class="inline-flex items-center justify-center w-8 h-8 !p-0 hoverBg js-options">
+                <img src="src/images/options.svg" alt="options">
+            </figure>
+        </div>
+    </section>
+`;
+
+// abrir histórico completo
+const historyItemLoad = 
+`
+    <div class="space-y-1 hidden items-center gap-6 cursor-pointer hoverBg js-buttonLoad">
+        <img class="ml-[6px] js-rotate90" src="src/images/options.svg" alt="Carregar mais">
+        <p class="capitalize text-gray-400 text-base">Carregar Mais</p>
+    </div>
+    <section class="hidden space-y-1 js-sectionLoad">
+        <div class="flex items-center gap-5 cursor-pointer hoverBg">
+            <div class="space-y-[2px] *:w-4 *:h-[2px] *:bg-gray-500">
+                <div></div>
+                <div></div>
+                <div class="!w-[10px]"></div>
+            </div>
+            <p class="flex-1 text-gray-400 text-base">${message}</p>
+            <figure class="inline-flex items-center justify-center w-8 h-8 !p-0 hoverBg js-options">
+                <img src="src/images/options.svg" alt="options">
+            </figure>
+        </div>
+    </section>
+`;
