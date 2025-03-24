0f9aca8faf8c15b5691ac7a2b512e74f7865db6d
Leozinho2831 <leoferraz1818@gmail.com>
2025-03-24 16:51:49 -0300

    initial logic menu

--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -1,6 +1,7 @@
 // menu nav
 const menu = document.querySelector('.js-menu');
 const gridContainer = document.querySelector('.grid-container');
+const sectionMenu = document.querySelector('.js-sectionMenu');
 
 const classOpenMenu = 'open-menu';
 const widthScreen = document.body.clientWidth;
@@ -12,7 +13,17 @@ if(widthScreen > 640){
 }
 
 menu.onclick = () => {
-    gridContainer.classList.toggle(classOpenMenu);
+    if(!gridContainer.classList.contains(classOpenMenu)){
+        gridContainer.classList.add(classOpenMenu);
+
+        sectionMenu.classList.add('grid');
+        sectionMenu.classList.remove('hidden');
+    } else {
+        gridContainer.classList.remove(classOpenMenu);
+
+        sectionMenu.classList.remove('grid');
+        sectionMenu.classList.add('hidden');
+    }
 }
 
 // menu options
@@ -20,10 +31,12 @@ const menuOptions = document.querySelector('.js-menuOptions');
 const buttonsOptions = document.querySelectorAll('.js-options');
 
 document.onclick = (event) => {
-    const verify = !menuOptions.contains(event.target) && !event.target.closest('.js-options');
+    if(menuOptions){
+        const verify = !menuOptions.contains(event.target) && !event.target.closest('.js-options');
 
-    if(verify){
-        menuOptions.classList.add('hidden');
+        if(verify){
+            menuOptions.classList.add('hidden');
+        }
     }
 }
 
