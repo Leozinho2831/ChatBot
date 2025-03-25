84c0f28009f2fdc710af59a6702aac5c8497e602
Leozinho2831 <leoferraz1818@gmail.com>
2025-03-25 13:52:01 -0300

    added bg black

--- src/javascript/menuOptions.js
+++ src/javascript/menuOptions.js
@@ -1,16 +1,23 @@
 document.onclick = (event) => {
     const menuOptions = document.querySelector('.js-menuOptions');
+    const bgBlack = document.querySelector('.js-bgBlack');
     // usado para pegar o botão de clique e não precisar criar um evento de clique para ele, assim não precisando clicar duas vezes no botão
     const buttonOption = event.target.closest('.js-options');
 
     function openMenuOptions(event){
-        const itemEvent = event.currentTarget;
-        const distanceTop = itemEvent.offsetTop;
+        const itemEvent = event.target;
+
+        const distanceTop = itemEvent.offsetTop - 10;
         
-        menuOptions.style.cssText = `top: ${distanceTop}px`;
+        if(widthScreen > 640){
+            menuOptions.style.cssText = `top: ${distanceTop}px`;
+        } else {
+            bgBlack.classList.remove('hidden');
+        }
     
         if(!menuOptions.classList.contains('hidden')){
             menuOptions.classList.add('hidden');
+            bgBlack.classList.add('hidden');
         } else {
             menuOptions.classList.remove('hidden');
         }
@@ -20,9 +27,10 @@ document.onclick = (event) => {
         openMenuOptions(event);
     }
 
-    const verify = menuOptions && !menuOptions.contains(event.target) && !event.target.closest('.js-options');
+    const verify = menuOptions && !menuOptions.contains(event.target) && !event.target.closest('.js-options') && event.target === bgBlack;
 
     if(verify){
         menuOptions.classList.add('hidden');
+        bgBlack.classList.add('hidden');
     }
 }
