a71525fbda19f192ec6e839a37892457bc4fa5e5
Leozinho2831 <leoferraz1818@gmail.com>
2025-03-24 22:39:51 -0300

    initial correct logic in open menu

--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -27,35 +27,37 @@ menu.onclick = () => {
     }
 }
 
-// menu options
-const menuOptions = document.querySelector('.js-menuOptions');
-const buttonsOptions = document.querySelectorAll('.js-options');
-
+// menu options. Tudo dentro do document.onclick para quando criar os itens selecionar o item e salva na memória
 document.onclick = (event) => {
+    const menuOptions = document.querySelector('.js-menuOptions');
+    const buttonsOptions = document.querySelectorAll('.js-options');
+    console.log(menuOptions, buttonsOptions);
+
+    function openMenuOptions(event){
+        const itemEvent = event.currentTarget;
+        const distanceTop = itemEvent.offsetTop;
+        
+        menuOptions.style.cssText = `top: ${distanceTop}px`;
+    
+        if(!menuOptions.classList.contains('hidden')){
+            menuOptions.classList.add('hidden');
+        } else {
+            menuOptions.classList.remove('hidden');
+        }
+    }
+    
+    buttonsOptions.forEach((buttonOptions) => {
+        buttonOptions.onclick = (event) => {
+            console.log(1);
+            openMenuOptions(event);
+        }
+    });     
+
     if(menuOptions){
-        const verify = !menuOptions.contains(event.target) && !event.target.closest('.js-options');
+        const verify = menuOptions && !menuOptions.contains(event.target) && !event.target.closest('.js-options');
 
         if(verify){
             menuOptions.classList.add('hidden');
         }
     }
 }
-
-function openMenuOptions(event){
-    const itemEvent = event.currentTarget;
-    const distanceTop = itemEvent.offsetTop;
-    
-    menuOptions.style.cssText = `top: ${distanceTop}px`;
-
-    if(!menuOptions.classList.contains('hidden')){
-        menuOptions.classList.add('hidden');
-    } else {
-        menuOptions.classList.remove('hidden');
-    }
-}
-
-buttonsOptions.forEach((buttonOptions) => {
-    buttonOptions.onclick = (event) => {
-        openMenuOptions(event);
-    }
-});
