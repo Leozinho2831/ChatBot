59757dd33a367fd84bd2c87a20e1cb8b23b1ad52
Leozinho2831 <leoferraz1818@gmail.com>
2025-03-26 11:00:20 -0300

    import components

--- src/javascript/history.js
+++ src/javascript/history.js
@@ -1,9 +1,8 @@
+import { historyItem, firstMessage, historyItemPlus, historyItemLoad } from './components.js';
+
 const nav = document.querySelector('.js-nav');
 const form = document.querySelector('.js-form');
-
 const input = document.querySelector('.js-input');
-const buttonMic = document.querySelector('.js-buttonMic');
-const buttonSend = document.querySelector('.js-buttonSend');
 
 function openMoreHistory(){
     const buttonPlus = document.querySelector('.js-buttonPlus');
@@ -77,53 +76,6 @@ function openMoreHistory(){
     }
 }
 
-// components
-const historyItem = (message) => {
-    return  `<div class="flex items-center gap-5 cursor-pointer hoverBg js-message">
-                <div class="space-y-[2px] *:w-4 *:h-[2px] *:bg-gray-500">
-                    <div></div>
-                    <div></div>
-                    <div class="!w-[10px]"></div>
-                </div>
-                <p class="flex-1 text-gray-400 text-base">${message}</p>
-                <figure class="inline-flex items-center justify-center w-8 h-8 !p-0 hoverBg js-options">
-                    <img src="src/images/options.svg" alt="options">
-                </figure>
-            </div>
-            `;
-}
-
-const firstMessage = (message) => {
-    return  `<b class="px-3 font-semibold text-gray-200 text-lg">Recentes</b>
-            ${historyItem(message)}
-            `;
-}
-
-// criar botão abrir mais histórico
-const historyItemPlus = (message) => {
-    return  `<div class="flex items-center gap-5 cursor-pointer hoverBg js-buttonPlus">
-                <img class="w-4 h-4 js-rotate180" src="src/images/arrow-menu.svg" alt="Mais">
-                <p class="text-gray-400 text-base">Mais</p>
-            </div>
-            <section class="hidden space-y-1 js-sectionPlus">
-                ${historyItem(message)}
-            </section>
-            `;
-}
-
-// criar botão abrir histórico completo
-const historyItemLoad = (message) => {
-    return  `
-            <div class="space-y-1 flex items-center gap-6 cursor-pointer hoverBg js-buttonLoad">
-                <img class="ml-[6px] js-rotate90" src="src/images/options.svg" alt="Carregar mais">
-                <p class="capitalize text-gray-400 text-base">Carregar Mais</p>
-            </div>
-            <section class="hidden space-y-1 js-sectionLoad">
-                ${historyItem(message)}
-            </section>
-            `;
-}
-
 function createMessage(){
     const navChild = nav.querySelectorAll('.js-message').length;
 
@@ -156,16 +108,6 @@ function createMessage(){
     }
 
     input.value = '';
-}
-
-input.oninput = () => {
-    if(input.value){
-        buttonMic.classList.add('hidden');
-        buttonSend.classList.remove('hidden');
-    } else {
-        buttonSend.classList.add('hidden');
-        buttonMic.classList.remove('hidden');
-    }
 }
 
 form.onsubmit = (event) => {
