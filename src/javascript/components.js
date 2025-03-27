b9e4e2c4291f260068fec343c2fa1c3b7536719f
Leozinho2831 <leoferraz1818@gmail.com>
2025-03-27 00:53:50 -0300

    new style in components new function openHistory

--- src/javascript/components.js
+++ src/javascript/components.js
@@ -1,6 +1,6 @@
 // components
 export const historyItem = (message) => {
-    return  `<div class="flex items-center gap-5 cursor-pointer hoverBg js-message">
+    return  `<div class="flex flex-1 items-center gap-5 cursor-pointer hoverBg js-message">
                 <div class="space-y-[2px] *:w-4 *:h-[2px] *:bg-gray-500">
                     <div></div>
                     <div></div>
@@ -22,7 +22,7 @@ export const firstMessage = (message) => {
 
 // criar botão abrir mais histórico
 export const historyItemPlus = (message) => {
-    return  `<button class="flex items-center gap-5 cursor-pointer hoverBg js-buttonPlus">
+    return  `<button class="w-full flex items-center gap-5 cursor-pointer hoverBg js-buttonPlus">
                 <img class="w-4 h-4 js-rotate180" src="src/images/arrow-menu.svg" alt="Mais">
                 <p class="text-gray-400 text-base">Mais</p>
             </button>
@@ -35,7 +35,7 @@ export const historyItemPlus = (message) => {
 // criar botão abrir histórico completo
 export const historyItemLoad = (message) => {
     return  `
-            <button class="space-y-1 flex items-center gap-6 cursor-pointer hoverBg js-buttonLoad">
+            <button class="space-y-1 w-full flex items-center gap-6 cursor-pointer hoverBg js-buttonLoad">
                 <img class="ml-[6px] js-rotate90" src="src/images/options.svg" alt="Carregar mais">
                 <p class="capitalize text-gray-400 text-base">Carregar Mais</p>
             </button>
@@ -43,4 +43,44 @@ export const historyItemLoad = (message) => {
                 ${historyItem(message)}
             </section>
             `;
+}
+
+// open history
+export function openHistory(element){
+    if(element.classList.contains('hidden')){
+        element.classList.remove('hidden');
+        element.classList.add('block');
+    } else {
+        element.classList.remove('block');
+        element.classList.add('hidden');
+    }
+        
+    const button = element.previousElementSibling;
+    const classRotate180 = 'rotate180'; 
+
+    if(button.classList.contains('js-buttonPlus')){
+        const textPlus = 'Mais';
+        const textLess = 'Menos';
+                
+        button.children[0].classList.toggle(classRotate180);
+    
+        if(button.children[1].textContent === textPlus){
+            button.children[1].textContent = textLess;
+        } else {
+            button.children[1].textContent = textPlus;
+        }
+
+    } else if(button.classList.contains('js-buttonLoad')){
+        const textLoadPlus = 'Carregar Mais';
+        const textLoadLess = 'Carregar Menos';
+
+        button.children[0].classList.toggle(classRotate180);
+
+        if(button.children[1].textContent === textLoadPlus){
+            button.children[1].textContent = textLoadLess;
+        } else {
+            button.children[1].textContent = textLoadPlus;
+        }
+
+    }  
 }
