9c7b8a3cf520bd471d8cb5accc98937f3937de28
Leozinho2831 <leoferraz1818@gmail.com>
2025-03-27 00:54:04 -0300

    new style to openMessage

--- src/javascript/openMessageUser.js
+++ src/javascript/openMessageUser.js
@@ -1,1 +1,12 @@
-const openMessage = document.querySelector('.js-openMessageUser');
+const openMessages = document.querySelectorAll('.js-openMessageUser');
+
+openMessages.forEach((openMessage) =>{
+    openMessage.onclick = () => {
+        const classRotate180 = 'rotate180';
+        openMessage.classList.toggle(classRotate180);
+    
+        const text = openMessage.previousElementSibling
+
+        text.classList.toggle('truncate');
+    }
+});
