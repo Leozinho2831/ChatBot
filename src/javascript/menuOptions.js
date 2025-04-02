4d512fa210a899f6cee200317ec5d15e71e193f7
Leozinho2831 <leoferraz1818@gmail.com>
2025-04-02 11:43:52 -0300

    remove functions of menu options

--- src/javascript/menuOptions.js
+++ src/javascript/menuOptions.js
@@ -29,61 +29,6 @@ document.addEventListener('click', (event) => {
         openMenuOptions(event);
     }
 
-    // functions of menu options
-    const buttonRename = menuOptions.children[0];
-    const buttonDelete = menuOptions.children[1];
-    
-    function renameHistory(textRename) {
-        menuOptions.classList.add('hidden');
-        bgBlack.classList.add('hidden');
-    
-        const input = document.createElement('input');
-        input.type = 'text';
-        input.classList.add('toggleTextHistory');
-        input.value = textRename.textContent;
-    
-        textRename.replaceWith(input);
-        input.focus();
-    
-        function replaceInput() {
-            // verifica se o item não está na árvore do dom
-            if (!input.parentNode) return;
-
-            textRename.textContent = input.value;
-            input.replaceWith(textRename);
-    
-            // Remover os eventos para evitar chamadas duplicadas
-            input.removeEventListener('blur', replaceInput);
-            input.removeEventListener('keydown', handleKeydown);
-        }
-    
-        function handleKeydown(event) {
-            if (event.key === 'Enter') {
-                // se for pelo enter remove o evento de blur
-                input.removeEventListener('blur', replaceInput);
-                replaceInput();
-            }
-        }
-    
-        input.addEventListener('blur', replaceInput);
-        input.addEventListener('keydown', handleKeydown);
-    }
-    
-    buttonRename.onclick = () => {
-        const textRename = buttonOption.previousElementSibling;
-        renameHistory(textRename);
-    };
-    
-    
-    buttonDelete.onclick = () => {
-        const containerHistory = buttonOption.parentElement;
-        containerHistory.remove();
-
-        if(nav.children.length === 1){
-            nav.innerHTML = '';
-        }
-    }
-
     // fechar menu option
     const verify = (!event.target.closest('.js-menuOptions') && !buttonOption) || menuOptions.children[1].contains(event.target);    
     
