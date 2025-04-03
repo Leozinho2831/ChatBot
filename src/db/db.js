a06bf71eb368e6d3c381618caebca8e803c3fd7b
Leozinho2831 <leoferraz1818@gmail.com>
2025-04-03 16:36:00 -0300

    start db

--- src/db/db.js
+++ src/db/db.js
@@ -1,4 +1,11 @@
-let db;
-
 // aqui faço a requisição do banco de dados, dando um nome para buscar e a versão dele, caso o banco não exisitir ele irá criar um indexedDB de dados com esse nome
 const requestDB = indexedDB.open("bancoChat", 1);
+
+requestDB.onsuccess = (event) => {
+    let db = event.target.result;
+    console.log('Banco de dados aberto com sucesso!', db)
+}
+
+requestDB.onerror = (event) => {
+    console.log('Erro ao abrir o banco de dados:', event.target.error);
+}
