5850e015b662b19446dd00d9a1f9710d8087a09b
Leozinho2831 <leoferraz1818@gmail.com>
2025-04-03 16:46:24 -0300

    create first store

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,22 @@
 // aqui faço a requisição do banco de dados, dando um nome para buscar e a versão dele, caso o banco não exisitir ele irá criar um indexedDB de dados com esse nome
 const requestDB = indexedDB.open("bancoChat", 1);
 
-requestDB.onsuccess = (event) => {
+requestDB.onupgradeneeded = (event) => {
     let db = event.target.result;
-    console.log('Banco de dados aberto com sucesso!', db)
+
+    // cria uma tabela, como uma caixa para gaurdar os itens
+    db.createObjectStore('nav', { 
+        keyPath: 'id',
+        autoIncrement: true,
+    });
 }
 
+// executa se abrir o banco com sucesso
+requestDB.onsuccess = (event) => {
+    
+}
+
+// executa se der erro para abrir o banco
 requestDB.onerror = (event) => {
     console.log('Erro ao abrir o banco de dados:', event.target.error);
 }
